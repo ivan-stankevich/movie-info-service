8db992f39b50ab9fc8098cdf4d2fd35f8b0afb4c
Ivan Stankevich <stankevichivann@yandex.by>
2022-11-13 21:26:14 +0300

    Initial commit

---
+++ src/main/java/com/api/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,65 @@
+package com.api.movieinfoservice.resources;
+
+import com.api.movieinfoservice.models.Movie;
+import com.api.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+
+        MovieSummary movieSummary = webClientBuilder.build()
+                .get()
+                .uri("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey)
+                .retrieve()
+                .bodyToMono(MovieSummary.class)
+                .block();
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
